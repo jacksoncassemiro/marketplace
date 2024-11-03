79ee52c4f3006f44db149aeb754b251664955bca
Jackson Cassemiro <jacksondgls@live.com>
2024-11-03 11:54:53 -0300

    chore: removido propriedade no html
    Signed-off-by: Jackson Cassemiro <jacksondgls@live.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR" suppressHydrationWarning>
+    <html lang="pt-BR">
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           <div className="flex-1 w-full flex flex-col gap-20 items-center">
