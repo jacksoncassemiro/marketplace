056d7d332014d4cd3de86e7c55e9a058f98896a5
Jackson Cassemiro <jacksondgls@live.com>
2024-11-05 19:21:37 -0300

    prev: ajuste parcial da dependência do mantine
    Signed-off-by: Jackson Cassemiro <jacksondgls@live.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { MantineProvider, ColorSchemeScript } from '@mantine/core';
+import { theme } from '../theme';
 import HeaderAuth from "@/components/header-auth";
 import Link from "next/link";
 import "./globals.css";
@@ -19,23 +21,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          <div className="flex-1 w-full flex flex-col gap-20 items-center">
-            <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-              <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                <div className="flex gap-5 items-center font-semibold">
-                  <Link href={"/"}>Next.js Supabase Starter</Link>
-                </div>
-                <HeaderAuth />
-              </div>
-            </nav>
-            <div>layout geral</div>
-            <div className="flex flex-col gap-20 max-w-5xl p-5">
-              {children}
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className="">
+        <MantineProvider theme={theme} defaultColorScheme="light" withCssVariables>
+          <div className="min-h-screen flex flex-col items-center">
+            <div className="flex gap-5 items-center font-semibold">
+              <Link href={"/"}>Next.js Supabase Starter</Link>
             </div>
+            <HeaderAuth />
           </div>
-        </main>
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
