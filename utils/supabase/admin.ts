fa57bcde5a87b15f720e69c96d5c5fbf38dac2d7
Jackson Cassemiro <jacksondgls@live.com>
2024-11-10 17:39:56 -0300

    feat: criado o client para conexão de nível de administrador ao banco de dados
    Signed-off-by: Jackson Cassemiro <jacksondgls@live.com>

---
+++ utils/supabase/admin.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Nota: supabaseAdmin usa o SERVICE_ROLE_KEY que você deve usar somente em um contexto seguro do lado do servidor
+// pois ele tem privilégios de administrador e substitui as políticas RLS!
+export const createServiceRoleClient = () => {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  );
+};
