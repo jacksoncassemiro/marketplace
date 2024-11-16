8b11164f640ee239a771132aade9dde9ed1951f4
Jackson Cassemiro <jacksondgls@live.com>
2024-11-16 17:46:10 -0300

    feat: adição do favicon
    Signed-off-by: Jackson Cassemiro <jacksondgls@live.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,12 @@ export default function RootLayout({
 			<html lang="pt-BR" suppressHydrationWarning>
 				<head>
 					<ColorSchemeScript />
+					<link rel="icon" type="image/png" href="/./public/favicon/favicon-96x96.png" sizes="96x96" />
+					<link rel="icon" type="image/svg+xml" href="/./public/favicon/favicon.svg" />
+					<link rel="shortcut icon" href="/./public/favicon/favicon.ico" />
+					<link rel="apple-touch-icon" sizes="180x180" href="/./public/favicon/apple-touch-icon.png" />
+					<meta name="apple-mobile-web-app-title" content="Marketplace PI" />
+					<link rel="manifest" href="/./public/favicon/site.webmanifest" />
 				</head>
 				<body>
 					<MantineProvider
