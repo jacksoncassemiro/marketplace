8233ab2e7885ddb9d8c0eb2485c1d826773a0d64
Jackson Cassemiro <jacksondgls@live.com>
2024-11-22 19:49:31 -0300

    fix: corrigido bugs com a troca do tema
    Signed-off-by: Jackson Cassemiro <jacksondgls@live.com>

--- components/form/auth/formInputsSignIn.tsx
+++ components/form/auth/formInputsSignIn.tsx
@@ -1,5 +1,7 @@
-import { ButtonLink } from "@/components/ui/buttonLink";
-import { Button, Input, Text, Title } from "@mantine/core/lib/components";
+"use client";
+
+import { Anchor } from "@/components/ui/anchor";
+import { Button, Input, Text, Title } from "@mantine/core";
 import { useFormStatus } from "react-dom";
 
 export const FormInputsSignIn = () => {
@@ -8,43 +10,46 @@ export const FormInputsSignIn = () => {
     <>
       <Title
         className="text-2xl font-medium"
-      >Sign in</Title>
+      >
+				Entrar
+			</Title>
 			<Text
 				size="sm"
 			>
-				Don't have an account?{" "}
-				<ButtonLink
+				Não tem uma conta?{" "}
+				<Anchor
 					className="text-foreground font-medium underline"
 					href="/criar-conta"
 				>
-					Sign up
-				</ButtonLink>
+					Cadastrar-se
+				</Anchor>
 			</Text>
 			<div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
 				<Input.Wrapper label="Email">
 					<Input type="email" />
 				</Input.Wrapper>
-				<div className="flex justify-between items-center">
-					<Label htmlFor="password">Password</Label>
-					<ButtonLink
-						className="text-xs text-foreground underline"
-						href="/alterar-senha"
-					>
-						Forgot Password?
-					</ButtonLink>
-				</div>
-				<Input
-					type="password"
-					name="password"
-					placeholder="Your password"
-					required
-				/>
+				<Input.Wrapper label="Senha">
+					<Input
+						type="password"
+						name="password"
+						placeholder="Your password"
+						required
+					/>
+				</Input.Wrapper>
 				<Button
           type="submit"
           loading={pending}
         >
-					Sign in
+					Entrar
 				</Button>
+				<div className="flex justify-between items-center">
+					<Anchor
+						className="text-xs text-foreground underline"
+						href="/alterar-senha"
+					>
+						Esqueceu sua senha??
+					</Anchor>
+				</div>
 			</div>
     </>
   );
