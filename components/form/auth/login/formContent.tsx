d59864aa607cbf5bde560123c9e6da3d47e18d35
Jackson Cassemiro <jacksondgls@live.com>
2024-11-25 19:10:31 -0300

    feat: ajustado form com input exibindo os erros
    Signed-off-by: Jackson Cassemiro <jacksondgls@live.com>

--- components/form/auth/login/formContent.tsx
+++ components/form/auth/login/formContent.tsx
@@ -1,12 +1,34 @@
+"use client";
+
 import { actionSignIn } from "@/app/(paginas-de-autenticacao)/authUtils";
 import { FormInputsSignIn } from "@/components/form/auth/login/formInputs";
 import { FormRoot } from "@/components/form/formRoot";
 
 export const FormContent = () => {
+	const validate = {
+		email: (value: string) => {
+			if (!value) {
+				return "Campo obrigatório";
+			}
+			if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+				return "Email inválido";
+			}
+			return null;
+		},
+		password: (value: string) => {
+			if (!value) {
+				return "Campo obrigatório";
+			}
+			return null;
+		},
+	};
 	return (
 		<FormRoot
 			formAction={actionSignIn}
 			className="relative flex flex-col gap-mantine-sm"
+			validate={validate}
+			initialValues={{ email: "", password: "" }}
+			nameForm="signIn"
 		>
 			<FormInputsSignIn />
 		</FormRoot>
--- components/form/auth/login/formInputs.tsx
+++ components/form/auth/login/formInputs.tsx
@@ -5,13 +5,16 @@ import { Box, Button, Input, LoadingOverlay, Stack, Text, Title } from "@mantine
 import { useDisclosure } from "@mantine/hooks";
 import { useEffect } from "react";
 import { useFormStatus } from "react-dom";
+import { useFormContext } from "@/components/form/formRoot";
 
 export const FormInputsSignIn = () => {
+	const form = useFormContext();
   const { pending } = useFormStatus();
 	const [visible, { toggle }] = useDisclosure(true);
 	useEffect(() => {
 		toggle();
 	}, [pending]);
+
   return (
     <>
 			<LoadingOverlay visible={visible} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />
@@ -37,14 +40,19 @@ export const FormInputsSignIn = () => {
 				gap="lg"
 			>
 				<Box className="flex flex-col gap-mantine-xs">
-					<Input.Wrapper label="E-mail">
-						<Input type="email" />
+					<Input.Wrapper label="E-mail" error={form.errors.email}>
+						<Input
+							type="text"
+							key={form.key('email')}
+        			{...form.getInputProps('email')}
+						/>
 					</Input.Wrapper>
-					<Input.Wrapper label="Senha">
+					<Input.Wrapper label="Senha" error={form.errors.password}>
 						<Input
 							type="password"
 							name="password"
-							required
+							key={form.key('password')}
+        			{...form.getInputProps('password')}
 						/>
 					</Input.Wrapper>
 				</Box>
