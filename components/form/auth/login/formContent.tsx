279a43433a5bb3182bd5602b6e6ab1a6f1801516
Jackson Cassemiro <jacksondgls@live.com>
2024-12-05 21:43:39 -0300

    chore: organizado estrutura para os schemas do projeto
    Signed-off-by: Jackson Cassemiro <jacksondgls@live.com>

--- components/form/auth/login/formContent.tsx
+++ components/form/auth/login/formContent.tsx
@@ -3,31 +3,15 @@
 import { actionSignIn } from "@/app/(paginas-de-autenticacao)/authUtils";
 import { FormInputsSignIn } from "@/components/form/auth/login/formInputs";
 import { FormRoot } from "@/components/form/formRoot";
+import { initialValues, validate } from "@/schemas/login/loginSchema";
 
 export const FormContent = () => {
-	const validate = {
-		email: (value: string) => {
-			if (!value) {
-				return "Campo obrigatório";
-			}
-			if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
-				return "Email inválido";
-			}
-			return null;
-		},
-		password: (value: string) => {
-			if (!value) {
-				return "Campo obrigatório";
-			}
-			return null;
-		},
-	};
 	return (
 		<FormRoot
 			formAction={actionSignIn}
 			className="flex flex-col gap-mantine-sm"
 			validate={validate}
-			initialValues={{ email: "", password: "" }}
+			initialValues={initialValues}
 			nameForm="signIn"
 		>
 			<FormInputsSignIn />
---
+++ schemas/login/loginSchema.ts
@@ -0,0 +1,22 @@
+export const validate = {
+  email: (value: string) => {
+    if (!value) {
+      return "Campo obrigatório";
+    }
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+      return "Email inválido";
+    }
+    return null;
+  },
+  password: (value: string) => {
+    if (!value) {
+      return "Campo obrigatório";
+    }
+    return null;
+  },
+};
+
+export const initialValues = {
+  email: "",
+  password: "",
+}
